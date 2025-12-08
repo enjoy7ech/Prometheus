921f4b6166fe019d3b841630e070173cc293ed76
zhenxiang <zxdong@agioe.com>
2025-12-08 18:01:40 +0800

    .

---
+++ assets/dev/static/chunks/pages__app_2da965e7._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/37518_next_dist_compiled_80fac0e2._.js",
+  "static/chunks/37518_next_dist_shared_lib_b32a1e38._.js",
+  "static/chunks/37518_next_dist_client_d653e5f6._.js",
+  "static/chunks/37518_next_dist_ce9d2a93._.js",
+  "static/chunks/37518_next_app_7ddee372.js",
+  "static/chunks/[next]_entry_page-loader_ts_644f91d3._.js",
+  "static/chunks/76102_react-dom_44795136._.js",
+  "static/chunks/node_modules__pnpm_7136f466._.js",
+  "static/chunks/[root-of-the-server]__45f039c3._.js"
+],
+    source: "entry"
+});
